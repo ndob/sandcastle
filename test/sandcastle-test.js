58772daee4bc59eac78945e880e7282b9dc0e922
ndob <dobniki@gmail.com>
2013-12-01 14:27:22 +0200

    Added possibility to use JavaScript strict mode, when running scripts.

--- test/sandcastle-test.js
+++ test/sandcastle-test.js
@@ -212,4 +212,22 @@ exports.tests = {
       finished();
     }
   },
+  'enforce js-strict mode, when running scripts': function(finished, prefix) {    
+    var sandcastle = new SandCastle({useStrictMode: true});
+
+    var script = sandcastle.createScript("\
+        exports.main = function() {\n\
+          globalObjectAccidentalPollution = true; \
+          exit(0); \
+        }\n\
+      "
+    );
+
+    script.on('exit', function(err, result) {
+      notEqual(-1, err.toString().indexOf("globalObjectAccidentalPollution is not defined"), prefix);
+      sandcastle.kill();
+      finished();
+    });
+    script.run();
+  },
 }
